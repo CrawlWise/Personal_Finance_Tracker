0d75322cef158fb2b75616f73cb9b57801384515
Crawler <alberteromosele0@gmail.com>
2023-10-26 18:16:13 +0100

    index Page structure completed

--- PFT/account/views.py
+++ PFT/account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def app(request):
+    return render(request, 'app.html')
 def login(request):
     return render(request, 'login.html')
 
